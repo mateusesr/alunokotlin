958fd760bfd716a10f02c921ad878ec9b10cdab8
Mateus Esr <mateusesr1@gmail.com>
2023-10-29 19:09:46 -0300

    commit inicial

---
+++ src/main/kotlin/br/upf/sistema_alunos/SistemaAlunosApplication.kt
@@ -0,0 +1,11 @@
+package br.upf.sistema_alunos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SistemaAlunosApplication
+
+fun main(args: Array<String>) {
+	runApplication<SistemaAlunosApplication>(*args)
+}
---
+++ src/test/kotlin/br/upf/sistema_alunos/SistemaAlunosApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.upf.sistema_alunos
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SistemaAlunosApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
